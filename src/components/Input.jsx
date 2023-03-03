699565b2e2e548a107df7be6169e45d32d4a0e9a
Alexandr Grisin <agrisin@griddynamics.com>
2023-03-03 18:45:19 +0200

    add input styling

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import './input.css';
 
 export const Input = ({ labelName, type, value, testId, handleOnChange }) => {
   return (
-    <label>
+    <label className={'input-label'}>
       {labelName}
       <input type={type} name="name" value={value} data-testid={testId} onChange={handleOnChange} />
     </label>
