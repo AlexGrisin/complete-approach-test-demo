4fa1f2cfc62be72ba9e13ce8bc1cb85a9c08c2d5
Alexandr Grisin <agrisin@griddynamics.com>
2023-03-06 19:25:17 +0200

    update login form stories

--- storefront/src/mocks/loginHandlerMock.js
+++
@@ -1,9 +0,0 @@
-export function handleLoginMock(userName, password) {
-  if (!userName || !password) {
-    return { statusMessage: 'Login error!', isError: true };
-  }
-  if (userName.includes('invalid') || password.includes('invalid')) {
-    return { statusMessage: 'Login failed: bad credentials', isError: true };
-  }
-  return { statusMessage: 'Login success', isError: false };
-}
--- storefront/src/stories/LoginForm.stories.jsx
+++ storefront/src/stories/LoginForm.stories.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { LoginForm } from '../components/LoginForm';
 import { within, userEvent } from '@storybook/testing-library';
 import { expect } from '@storybook/jest';
-import { handleLoginMock } from '../mocks/loginHandlerMock';
 
 export default {
   title: 'LoginApp/LoginForm',
@@ -12,10 +11,8 @@ export default {
 const Template = args => <LoginForm {...args} />;
 
 export const Empty = Template.bind({});
-Empty.args = { handleLogin: handleLoginMock };
 
 export const FilledForm = Template.bind({});
-FilledForm.args = { handleLogin: handleLoginMock };
 FilledForm.play = async ({ canvasElement }) => {
   const canvas = within(canvasElement);
   await userEvent.type(canvas.getByTestId('login'), 'email@provider.com');
@@ -26,7 +23,6 @@ FilledForm.play = async ({ canvasElement }) => {
 };
 
 export const EmptyForm = Template.bind({});
-EmptyForm.args = { handleLogin: handleLoginMock };
 EmptyForm.play = async ({ canvasElement }) => {
   const canvas = within(canvasElement);
   await userEvent.type(canvas.getByTestId('login'), 'email@provider.com');
