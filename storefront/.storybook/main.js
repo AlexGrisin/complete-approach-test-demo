e311ee7a7924a9061b2cd814737587b34bce8e44
Alexandr Grisin <agrisin@griddynamics.com>
2023-04-03 17:19:20 +0300

    add storybook coverage

--- storefront/.storybook/main.js
+++ storefront/.storybook/main.js
@@ -2,6 +2,7 @@ module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-a11y',
+    '@storybook/addon-coverage',
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
